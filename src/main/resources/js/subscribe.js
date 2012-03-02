abb56309aedb1a696dc008437e80dee8a16cb034
wkuo <wuichia@gmail.com>
2012-03-02 15:08:45 +0800

    MDCPD-70 Show error msg for all unfilled required field when Subscribe & Save button is hit

--- src/main/resources/js/subscribe.js
+++ src/main/resources/js/subscribe.js
@@ -12,18 +12,35 @@ AJS.toInit(function() {
 	      var mode = AJS.$("input:radio[name='mode']:checked").val();
 	      var secret = AJS.$("input#secret").val();
 	      var callback = AJS.$("input#callback").val();
-	      if (topic == "") {
-	    	  AJS.$("span#topic").show();
-	    	  AJS.$("input#topic").focus();  
-	    	  return false;  
+	      var username = AJS.$("input#username").val();
+	      var password = AJS.$("input#password").val();
+	      var error = false;
+	      if ('undefined' != typeof(mode) && mode == "") {
+	    	  AJS.$("span#mode").show();
+	    	  error = true;
 	      } 		
 	      if (secret == "") {
 	    	  AJS.$("span#secret").show();
 	    	  AJS.$("input#secret").focus();  
-		      return false;  
+	    	  error = true;
 	      } 		
-	      if ('undefined' != typeof(mode) && mode == "") {
-	    	  AJS.$("span#mode").show();
+	      if (topic == "") {
+	    	  AJS.$("span#topic").show();
+	    	  AJS.$("input#topic").focus();  
+	    	  error = true;
+	      } 		
+	      if (password == "") {
+	    	  AJS.$("span#password").show();
+	    	  AJS.$("input#password").focus();  
+	    	  error = true;
+	      } 		
+	      if (username == "") {
+	    	  AJS.$("span#username").show();
+	    	  AJS.$("input#username").focus();
+	    	  error = true;
+	      } 		
+	      if (error==true) {
+	    	  alert("error");
 	    	  return false;  
 	      } 		
 		
